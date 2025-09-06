e0030158192697a289218f1271aa0e84ed84dfe7
Ledanh3010 <nguyenledanh19@gmail.com>
2025-09-06 12:01:21 +0900

    upload 250609

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp1
 {
@@ -20,5 +12,48 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
+        private void ApplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"The message is: {this.DescriptionText.Text}");
+        }
+
+        private void ResetBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.checkBoxWeld.IsChecked = this.checkBoxAssembly.IsChecked = this.checkBoxLaser.IsChecked = this.checkBoxPlasma.IsChecked = this.checkBoxPurchase.IsChecked = false;
+            this.lengthBox.Text = "";
+           
+        }
+
+        private void Dropdown_Click(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox? combo = sender as ComboBox;
+            ComboBoxItem? comboItem = combo?.SelectedItem as ComboBoxItem;
+            if (comboItem != null)
+            {
+                this.lengthBox.Text = comboItem.Content.ToString();
+            }
+        }
+
+
+
+        private void checkedClick(object sender, RoutedEventArgs e)
+        {
+
+            CheckBox? checkBox = sender as CheckBox;
+            if (checkBox != null)
+            {
+                this.lengthBox.Text += checkBox.Content.ToString();
+                //MessageBox.Show($"Event: {e.Source}\n");
+                //MessageBox.Show($"Event: {e.RoutedEvent}\n");
+            }
+            
+        }
+
+        private void changeText(object sender, TextChangedEventArgs e)
+        {
+            this.supplierName.Text = this.nameMass.Text;
+        }
+
     }
+
 }
